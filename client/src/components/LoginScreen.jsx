073bfd38298fe63b155d0153f5ab915d79951909
Ibrahim <divinelecture05@example.com>
2026-06-21 10:53:01 +0530

    Make password field visible (type=text)

--- client/src/components/LoginScreen.jsx
+++ client/src/components/LoginScreen.jsx
@@ -96,7 +96,7 @@ function LoginScreen({ onLoginSuccess }) {
             <label htmlFor="password">Your Password</label>
             <input
               id="password"
-              type="password"
+              type="text"
               placeholder="Enter your password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
